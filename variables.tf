ae8fc74d35197bf28ecf87552dd044e1426afc18
Dmitriy Solodukha <dspv@me.com>
2022-04-20 14:38:14 +0300

    Initial commit (#2)
    * Initial commit
    * Remove capacity paramater
    * Try to put public containers on port 8080
    * network mode: awsvpc -> host
    * Try to use port 8080 for public services
    * Some fixes

---
+++ variables.tf
@@ -0,0 +1,142 @@
+variable vpc_id {}
+variable subnets {}
+variable image_name {}
+variable name_prefix {}
+variable domain_name {}
+variable aws_lb_arn {}
+variable ecr_region {}
+variable ecr_account_id {}
+variable security_groups {}
+variable aws_lb_certificate_arn {}
+variable logdna_lambda_logs_arn {}
+
+variable image_version {
+  type    = string
+  default = "latest"
+}
+variable wm_instance {
+  type    = string
+  default = "fargate"
+}
+variable standard_tags {
+  type = map(string)
+}
+variable cluster_name {
+  type = string
+}
+variable service_name {
+  type = string
+}
+variable container_cpu {
+  type    = number
+  default = 256
+}
+variable container_memory {
+  type    = number
+  default = 512
+}
+variable task_cpu {
+  type    = number
+  default = 256
+}
+variable task_memory {
+  type    = number
+  default = 512
+}
+variable service_port {
+  type    = number
+  default = 8080
+}
+variable external_port {
+  type    = number
+  default = 443
+}
+variable entrypoint {
+  type    = list(string)
+  default = null
+}
+variable command {
+  type    = list(string)
+  default = null
+}
+variable desired_count {
+  type    = number
+  default = 1
+}
+variable max_capacity {
+  type    = number
+  default = 1
+}
+variable min_capacity {
+  type    = number
+  default = 1
+}
+variable container_cpu_low_threshold {
+  type    = number
+  default = 60
+}
+variable container_cpu_high_threshold {
+  type    = number
+  default = 30
+}
+variable task_role_arn {
+  type    = string
+  default = null
+}
+variable health_check_grace_period_seconds {
+  type    = number
+  default = null
+}
+variable additional_containers {
+  description = "Additional containers definition"
+  default     = []
+}
+# variable s3_log_bucket {
+#   type = string
+# }
+variable public {
+  default = false
+}
+variable launch_type {
+  default = "FARGATE"
+}
+
+variable mount_points {
+  type = list(object({
+    containerPath = string
+    sourceVolume  = string
+    readOnly      = bool
+  }))
+  description = "Container mount points. This is a list of maps, where each map should contain a `containerPath` and `sourceVolume`"
+  default     = []
+}
+
+variable volumes {
+  type = list(object({
+    name = string
+    efs_volume_configuration = list(object({
+      file_system_id = string
+      root_directory = string
+    }))
+  }))
+  description = "Task volume definitions as list of configuration objects"
+  default     = []
+}
+
+variable environment {
+  type = list(object({
+    name  = any
+    value = any
+  }))
+  description = "List of Environment Variables"
+  default     = []
+}
+
+variable secrets {
+  type = list(object({
+    name      = string
+    valueFrom = string
+  }))
+  description = "List of Secrets"
+  default     = []
+}
