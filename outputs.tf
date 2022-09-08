8cb47b4f895cfa6faedaf759a7e9e1a25f35b1bd
Dmitriy <dspv@me.com>
2022-09-08 10:20:54 +0300

    Add Tagging & Remove unused outputs

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output service_port {
     value = var.service_port
 }
 
-output cloudwatch_log_group_name {
-    value = aws_cloudwatch_log_group.ecs_group.name
+output service_name {
+    value = var.service_name
+}
+
+output service_dns_name {
+    value = aws_service_discovery_service.main.name
 }
