6cc1b68b72d8cb819a4ad2a38f979fbe26335469
Dmitriy <dspv@me.com>
2022-09-13 18:03:28 +0300

    Add variables Description and defaults

--- variables.tf
+++ variables.tf
@@ -31,45 +31,57 @@ variable wenv {
   default = "fargate"
 }
 variable standard_tags {
-  type = map(string)
+  type        = map(string)
+  description = "Tags"
 }
 variable cluster_name {
-  type = string
+  type        = string
+  default     = "fargate"
+  description = "ECS Cluster name"
 }
 variable service_name {
-  type = string
+  type        = string
+  default     = "fargate"
+  description = "ECS Service name. Better to use with prefix"
 }
 variable container_cpu {
-  type    = number
-  default = 256
+  type        = number
+  default     = 256
+  description = "Container vCPU. 256 = 0.25 vCPU | 1024 = 1.0 vCPU | 4096 = 4.0 vCPU (max)"
 }
 variable container_memory {
-  type    = number
-  default = 512
+  type        = number
+  default     = 512
+  description = "Container Memory (RAM). 512 = 512 Mb | 1024 = 1024 Mb = 1.0 Gb | 8192 = 8192 Mb = 8.0 Gb (max)"
 }
 variable task_cpu {
-  type    = number
-  default = 256
+  type        = number
+  default     = 256
+  description = "Task vCPU. 256 = 0.25 vCPU | 1024 = 1.0 vCPU | 4096 = 4.0 vCPU (max)"
 }
 variable task_memory {
-  type    = number
-  default = 512
+  type        = number
+  default     = 512
+  description = "Task Memory (RAM). 512 = 512 Mb | 1024 = 1024 Mb = 1.0 Gb | 8192 = 8192 Mb = 8.0 Gb (max)"
 }
 variable service_port {
-  type    = number
-  default = 8080
+  type        = number
+  default     = 8080
+  description = "Container port. It is fine to leave the default value"
 }
 variable external_port {
-  type    = number
-  default = 443
+  type        = number
+  default     = 443
+  description = "You do not need to specify it"
 }
 variable entrypoint {
   type    = list(string)
   default = null
 }
 variable command {
-  type    = list(string)
-  default = null
+  type        = list(string)
+  default     = null
+  description = "Commands to run on launch"
 }
 variable desired_count {
   type    = number
@@ -92,25 +104,31 @@ variable container_cpu_high_threshold {
   default = 30
 }
 variable task_role_arn {
-  type    = string
-  default = null
+  type        = string
+  default     = null
+  description = "Task Role ARN"
 }
 variable health_check_grace_period_seconds {
-  type    = number
-  default = null
+  type        = number
+  default     = null
+  description = "Set 300 if your container needs to build / initialize something on launch"
 }
 variable additional_containers {
   description = "Additional containers definition"
   default     = []
 }
 variable public {
-  default = false
+  type        = bool
+  default     = false
+  description = "Set as true to use with public load balancer"
 }
 variable launch_type {
   default = "FARGATE"
 }
 variable retention_in_days {
-  default = 7
+  type        = number
+  default     = 7
+  description = "Amount of days to store service logs"
 }
 
 variable mount_points {
