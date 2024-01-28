b316961b6532103bd6e542553def3834a9cd15ab
Dmitriy Solodukha <dspv@me.com>
2024-01-28 21:59:30 +0200

    Remove ACM

--- data.tf
+++ data.tf
@@ -1,12 +1,2 @@
 data aws_region current {}
 data aws_caller_identity current {}
-
-data aws_acm_certificate main {
-    domain      = "*.${var.domain_name}"
-    statuses    = ["ISSUED"]
-    most_recent = true
-}
-
-data aws_route53_zone main {
-    name = var.domain_name
-}
