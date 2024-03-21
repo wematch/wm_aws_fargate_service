9e71ce00bd3c3025c2004332f9888f4123360370
Dmitriy Solodukha <dspv@me.com>
2024-03-21 09:51:46 +0200

    Add NODE_OPTIONS + Update LB TLS Policy

--- main.tf
+++ main.tf
@@ -114,7 +114,11 @@ module main_container_definition {
     {
       name  = "SERVICE_PORT"
       value = var.service_port
-    }    
+    },
+    {
+      name  = "NODE_OPTIONS"
+      value = "--max-old-space-size=${ var.container_memory }"
+    },
   ])
 
   log_configuration = {
@@ -170,7 +174,7 @@ resource aws_lb_listener main {
   load_balancer_arn = var.aws_lb_arn
   port              = var.external_port
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-Ext-2018-06"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
   certificate_arn   = var.aws_lb_certificate_arn
 
   default_action {
